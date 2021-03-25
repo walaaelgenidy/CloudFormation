319464133a191951bfbdc14d6a249b9e6b6d4756
walaaelgenidy <45464802+walaaelgenidy@users.noreply.github.com>
2021-03-25 18:11:11 +0200

    IAC-Final

--- project_iac/create.sh
+++ project_iac/create.sh
@@ -2,4 +2,5 @@ aws cloudformation create-stack \
 --stack-name $1 \
 --template-body file://$2 \
 --parameters file://$3 \
---region=us-east-2 
+--region=us-east-2 \
+--capabilities=CAPABILITY_NAMED_IAM
